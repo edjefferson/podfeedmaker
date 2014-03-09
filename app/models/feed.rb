99a26f20bef2557ae374e67a01154a35cb7c1e02
Ed Jefferson <e.jefferson@gmail.com>
2014-03-09 17:58:47 +0000

    basic version working

---
+++ app/models/feed.rb
@@ -0,0 +1,13 @@
+class Feed < ActiveRecord::Base
+  require 'nokogiri'
+  require 'open-uri'
+  def self.get_links
+    dir_listing = Nokogiri::HTML(open("http://somefoolnotion.com/private/"))
+    raw_links = dir_listing.xpath '//a'
+
+    links = raw_links.map { |link| link.attributes['href'].value}
+
+    links.select { |link| link.include? "mp3"}
+
+  end
+end
